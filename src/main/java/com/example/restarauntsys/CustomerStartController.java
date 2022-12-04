3d0d564547e57de5adde019ac261090fea7dc16e
Maksym <72620745+MaksimLik@users.noreply.github.com>
2022-12-04 14:59:48 +0100

    Add windows

--- src/main/java/com/example/restarauntsys/CustomerStartController.java
+++ src/main/java/com/example/restarauntsys/CustomerStartController.java
@@ -1,4 +1,20 @@
 package com.example.restarauntsys;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class CustomerStartController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
